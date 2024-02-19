ab30ae53ba27777a57b4d1ec1ca59907d9ffbc1a
David Salguera <dsalguera23@gmail.com>
2024-02-18 23:55:49 -0500

    Hero New Changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <div className="bg-gradient-to-r from-blue-500 to-purple-500 text-white text-center py-16">
+        <div className="bg-gradient-to-r from-blue-500 to-purple-500 text-white text-center py-16 h-full">
           <h1 className="text-4xl font-bold mb-4">Cinepedia</h1>
           <p className="text-lg">The place where you can stream and watch your favorite series and movies.</p>
           {/* Add other content or components for the hero section */}
